82b2a6833abbd773509e052614667c3602aa6cb5
Anthony Camacho <anthony.g.camacho@mail.com>
2017-02-04 14:17:23 -0800

    clean up and setup for save the pig

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+	require('jit-grunt')(grunt);
+	grunt.initConfig({
+		less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+					"../zimmybooks/wp-content/themes/zimmyb-site/style.css": "less/main.less",
+					"../zimmybooks/wp-content/themes/zimmyb-site/css/save-the-pig.css": "less/save-the-pig.less"
+        }
+      }
+    },
+		watch: {
+      styles: {
+        files: ['less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+	});
+	grunt.registerTask('default', ['less', 'watch']);
+}
